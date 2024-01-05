55ec23ba2b5905c411eb8e0bf33ad4c81d9cc1ce
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-05 02:00:12 +0000

    fixing pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""The module for pascal triangle
-"""
+'''The module for pascal triangle.
+'''
 
 
 def pascal_triangle(n):
-    """This creates a list the stores the binaries for the 
-    pascal triangle
-    """
+    '''This creates a list the stores the binaries for the 
+    pascal triangle.
+    '''
     if n <= 0:
         return []
     triangle = []
@@ -14,8 +14,8 @@ def pascal_triangle(n):
         row = [1]
         if triangle:
             last_row = triangle[-1]
-            for k in range(len(last_row) - 1):
-                row.append(last_row[k] + last_row[k + 1])
+            for j in range(len(last_row) - 1):
+                row.append(last_row[j] + last_row[j + 1])
             row.append(1)
         triangle.append(row)
     return triangle
