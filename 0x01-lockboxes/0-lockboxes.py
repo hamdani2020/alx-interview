a97481a225c3be54e919c08da2bdadd870749587
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-11 14:33:50 +0000

    fixed pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,7 +4,13 @@
 
 from collections import deque
 
+
 def canUnlockAll(boxes):
+    '''The function uses sets to keep track of seen and unseen
+    boxes. It uses the keys found in the first box or previous
+    box to unlock the other boxes. i.e it iterates through the boxes
+    using the keys found in each box.
+    '''
     n = len(boxes)
     seen_boxes = set([0])
     unseen_boxes = deque(boxes[0])
