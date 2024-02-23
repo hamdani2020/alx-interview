6bfda82a6deb171a77ad03da7ece8add9f7f3521
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-02-23 01:50:25 +0000

    Fixed pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,18 +4,12 @@
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates a n by n 2D matrix 90 degrees clockwise in place."""
-    n = len(matrix)
-    # Check if the matrix is empty or not square
-    if n == 0 or any(len(row) != n for row in matrix):
-        return
-
-    # Transpose the matrix
+    """Rotates an n x n 2D matrix 90 degrees clockwise in place."""
+    n = len(matrix)  # Assuming matrix is not empty and is square
     for i in range(n):
-        for j in range(i, n):
+        for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-    # Flip the matrix horizontally
     for i in range(n):
         for j in range(n // 2):
-            matrix[i][j], matrix[i][n - 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
+            matrix[i][j], matrix[i][n - 1 - j] = \
+                matrix[i][n - 1 - j], matrix[i][j]
